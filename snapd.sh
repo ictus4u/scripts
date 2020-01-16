f738f5d3b9ecd53ffa9b69eef7572139efb7f734
ictus4u <47392003+ictus4u@users.noreply.github.com>
2020-01-16 03:17:57 -0500

    Stop snapd.socket service wich was raising snapd

--- snapd.sh
+++ snapd.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 sudo systemctl enable snapd.service >/dev/null 2>&1
+sudo systemctl enable snapd.socket >/dev/null 2>&1
 sudo service snapd start
 
 sudo snap $@
 
 sudo service snapd stop
+sudo systemctl disable snapd.socket >/dev/null 2>&1
 sudo systemctl disable snapd.service >/dev/null 2>&1
